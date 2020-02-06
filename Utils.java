eaa2e770191145291d72a04a0ff6709d19ba4544
alanjoshua <41203962+alanjoshua@users.noreply.github.com>
2020-02-05 22:41:36 -0500

    Add files via upload

---
+++ Utils.java
@@ -0,0 +1,53 @@
+package Math;
+
+public class Utils {
+	
+	public static Vector projectPointToPlane(Vector e1, Vector e2, Vector p) {
+		Vector proj1 = e1.normalise().scalarMul(e1.normalise().dot(p));
+		Vector proj2 = e2.normalise().scalarMul(e2.normalise().dot(p));
+		Vector p_ = proj1.add(proj2);
+		return p_;
+	}
+	
+	public static float edge(Vector v1, Vector v2, Vector p) {
+		return (p.get(0) - v1.get(0)) * (v2.get(1) - v1.get(1)) - (p.get(0) - v1.get(0)) * (v2.get(0) - v1.get(0));
+	}
+	
+	public static boolean isPointInsideTriangle(Vector v1 ,Vector v2, Vector v3, Vector p, boolean shouldProject) {
+		boolean res = false;
+		Vector point = p;
+	
+		if(shouldProject) {
+			point = projectPointToPlane(v2.sub(v1),v2.sub(v3),p);
+		}
+		
+		float w1 = edge(v2, v3, point);
+		float w2 = edge(v1, v3, point);
+		float w3 = edge(v1, v2, point);
+		
+//		if(w1 >= 0 && w1 <= 1 && w2 >= 0 && w2 <= 1 && w3 >= 0 && w3 <= 1) {
+		if(w1 >= 0 && w2 >= 0 && w3 >= 0) {
+			res = true;
+		}
+		
+		return res;
+	}
+	
+	public static Vector getBoundingBox(Vector[] vertices) {
+		
+		int minX = Integer.MAX_VALUE;
+		int minY = Integer.MAX_VALUE;
+		int maxX = Integer.MIN_VALUE;
+		int maxY = Integer.MIN_VALUE;
+		
+		for(Vector vert : vertices) {
+			if(vert.get(0) < minX) minX = (int) vert.get(0);
+			if(vert.get(1) < minY) minY = (int) vert.get(1);
+			if(vert.get(0) > maxX) maxX = (int) vert.get(0);
+			if(vert.get(1) > maxY) maxY = (int) vert.get(1);
+		}
+		
+		return new Vector(new float[]{minX,minY,maxX,maxY});
+	}
+	
+}
--- Vector.java
+++ Vector.java
@@ -1,256 +1,433 @@
 package Math;
 
+import java.util.List;
+
 public class Vector {
-	
+
 	private float[] data;
-	private int size;
-	
-	public int getSize() {
-		return size;
+	private int numberOfDimensions;
+
+	public int getNumberOfDimensions() {
+		return numberOfDimensions;
 	}
-	
+
 	public Vector(float[] data) {
-			this.data = data;
-			this.size = data.length;
+		this.data = data;
+		this.numberOfDimensions = data.length;
 	}
-	
+
 	public Vector(int size) {
-		this.size = size;
+		this.numberOfDimensions = size;
 		data = new float[size];
-		for(int i = 0;i < size; i++) {
+		for (int i = 0; i < size; i++) {
 			data[i] = 0;
 		}
 	}
-	
+
 	public Vector(int size, float val) {
-		this.size = size;
+		this.numberOfDimensions = size;
 		data = new float[size];
-		for(int i = 0;i < size; i++) {
+		for (int i = 0; i < size; i++) {
 			data[i] = val;
 		}
 	}
 	
+	public Vector(Vector v) {
+		this.numberOfDimensions = v.getNumberOfDimensions();
+		data = new float[numberOfDimensions];
+		for (int i = 0; i < numberOfDimensions; i++) {
+			data[i] = v.get(i);
+		}
+	}
+
 	public Vector add(Vector v) {
-		float[] res;
-		if(getSize() == v.getSize()) {
-			res = new float[getSize()];
-			for(int i = 0;i < v.getSize();i++) {
-				res[i] = v.getData()[i] + this.getData()[i];
+		float[] res = null;
+		try {
+			if (getNumberOfDimensions() == v.getNumberOfDimensions()) {
+				res = new float[getNumberOfDimensions()];
+				for (int i = 0; i < v.getNumberOfDimensions(); i++) {
+					res[i] = v.getData()[i] + this.getData()[i];
+				}
+			} else {
+				throw new Exception("The vectors dont match in size");
 			}
-			return new Vector(res);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		else {
-			System.err.println("The vectors dont match in size. Returning same vector");
-			return new Vector(getData());
-		}
-		
+
+		return new Vector(res);
 	}
 	
-	public static Vector add(Vector u,Vector v) {
-		float[] res;
-		if(u.getSize() == v.getSize()) {
-			res = new float[u.getSize()];
-			for(int i = 0;i < v.getSize();i++) {
-				res[i] = v.getData()[i] + u.getData()[i];
+	public float getAngleBetweenVectors(Vector x) {
+		Vector v = this.normalise();
+		Vector w = x.normalise();
+		
+		float angle = v.dot(w);
+		return (float) Math.toDegrees(angle);
+	}
+
+	public static Vector add(Vector u, Vector v) {
+		float[] res = null;
+
+		try {
+			if (u.getNumberOfDimensions() == v.getNumberOfDimensions()) {
+				res = new float[u.getNumberOfDimensions()];
+				for (int i = 0; i < v.getNumberOfDimensions(); i++) {
+					res[i] = v.getData()[i] + u.getData()[i];
+				}
+			} else {
+				throw new Exception("The vectors dont match in size");
 			}
-			return new Vector(res);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return new Vector(res);
+
+	}
+
+	public float getNorm() {
+		float sum = 0;
+		for (float f : data) {
+			sum += (f * f);
+		}
+		float l = (float) Math.sqrt(sum);
+		return l;
+	}
+
+	public static double getLength(Vector v) {
+		double sum = 0;
+		for (float f : v.getData()) {
+			sum += (f * f);
+		}
+		double l = Math.sqrt(sum);
+		return l;
+	}
+
+	public Vector normalise() {
+		if(this.getNorm() != 0) {
+			return this.scalarMul((float) (1.0 / this.getNorm()));
 		}
 		else {
-			System.err.println("The vectors dont match in size. Returning same vector");
-			return new Vector(u.getData());
+			return this;
 		}
-		
 	}
-	
+
+	public static Vector normalise(Vector v) {
+		if(v.getNorm() != 0) {
+			return v.scalarMul((float) (1.0 / v.getNorm()));
+		}
+		else {
+			return v;
+		}
+	}
+
 	public Vector sub(Vector v) {
-		float[] res;
-		if(getSize() == v.getSize()) {
-			res = new float[getSize()];
-			for(int i = 0;i < v.getSize();i++) {
-				res[i] = v.getData()[i] - this.getData()[i];
+		float[] res = null;
+
+		try {
+			if (getNumberOfDimensions() == v.getNumberOfDimensions()) {
+				res = new float[getNumberOfDimensions()];
+				for (int i = 0; i < v.getNumberOfDimensions(); i++) {
+					res[i] = this.getData()[i] - v.getData()[i];
+				}
+			} else {
+				throw new Exception("The vectors dont match in size");
 			}
-			return new Vector(res);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		else {
-			System.err.println("The vectors dont match in size. Returning same vector");
-			return new Vector(getData());
-		}
-		
+		return new Vector(res);
 	}
-	
-	public static Vector sub(Vector u,Vector v) {
-		float[] res;
-		if(u.getSize() == v.getSize()) {
-			res = new float[u.getSize()];
-			for(int i = 0;i < v.getSize();i++) {
-				res[i] = v.getData()[i] - u.getData()[i];
+
+	public static Vector sub(Vector u, Vector v) {
+		float[] res = null;
+
+		try {
+			if (u.getNumberOfDimensions() == v.getNumberOfDimensions()) {
+				res = new float[u.getNumberOfDimensions()];
+				for (int i = 0; i < v.getNumberOfDimensions(); i++) {
+					res[i] = u.getData()[i] - v.getData()[i];
+				}
+			} else {
+				throw new Exception("The vectors dont match in size");
 			}
-			return new Vector(res);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		else {
-			System.err.println("The vectors dont match in size. Returning same vector");
-			return new Vector(u.getData());
-		}
-		
+		return new Vector(res);
+
 	}
-	
+
 	public Vector mul(Vector v) {
-		float[] res;
-		if(getSize() == v.getSize()) {
-			res = new float[getSize()];
-			for(int i = 0;i < v.getSize();i++) {
-				res[i] = v.getData()[i] * this.getData()[i];
+		float[] res = null;
+
+		try {
+			if (getNumberOfDimensions() == v.getNumberOfDimensions()) {
+				res = new float[getNumberOfDimensions()];
+				for (int i = 0; i < v.getNumberOfDimensions(); i++) {
+					res[i] = v.getData()[i] * this.getData()[i];
+				}
+			} else {
+				throw new Exception("The vectors dont match in size");
 			}
-			return new Vector(res);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		else {
-			System.err.println("The vectors dont match in size. Returning same vector");
-			return new Vector(getData());
-		}
-		
+
+		return new Vector(res);
+
 	}
-	
-	public static Vector mul(Vector u,Vector v) {
-		float[] res;
-		if(u.getSize() == v.getSize()) {
-			res = new float[u.getSize()];
-			for(int i = 0;i < v.getSize();i++) {
-				res[i] = v.getData()[i] * u.getData()[i];
+
+	public static Vector mul(Vector u, Vector v) {
+		float[] res = null;
+
+		try {
+			if (u.getNumberOfDimensions() == v.getNumberOfDimensions()) {
+				res = new float[u.getNumberOfDimensions()];
+				for (int i = 0; i < v.getNumberOfDimensions(); i++) {
+					res[i] = v.getData()[i] * u.getData()[i];
+				}
+			} else {
+				throw new Exception("The vectors dont match in size");
 			}
-			return new Vector(res);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		else {
-			System.err.println("The vectors dont match in size. Returning same vector");
-			return new Vector(u.getData());
-		}
-		
+
+		return new Vector(res);
+
 	}
-	
+
 	public Vector divide(Vector v) {
-		float[] res;
-		if(getSize() == v.getSize()) {
-			res = new float[getSize()];
-			for(int i = 0;i < v.getSize();i++) {
-				res[i] = v.getData()[i] + this.getData()[i];
+		float[] res = null;
+
+		try {
+			if (getNumberOfDimensions() == v.getNumberOfDimensions()) {
+				res = new float[getNumberOfDimensions()];
+				for (int i = 0; i < v.getNumberOfDimensions(); i++) {
+					res[i] = this.getData()[i] + v.getData()[i];
+				}
+			} else {
+				throw new Exception("The vectors dont match in size");
 			}
-			return new Vector(res);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		else {
-			System.err.println("The vectors dont match in size. Returning same vector");
-			return new Vector(getData());
-		}
-		
+		return new Vector(res);
 	}
-	
-	public static Vector divide(Vector u,Vector v) {
-		float[] res;
-		if(u.getSize() == v.getSize()) {
-			res = new float[u.getSize()];
-			for(int i = 0;i < v.getSize();i++) {
-				res[i] = v.getData()[i] / u.getData()[i];
+
+	public static Vector divide(Vector u, Vector v) {
+		float[] res = null;
+
+		try {
+			if (u.getNumberOfDimensions() == v.getNumberOfDimensions()) {
+				res = new float[u.getNumberOfDimensions()];
+				for (int i = 0; i < v.getNumberOfDimensions(); i++) {
+					res[i] = u.getData()[i] / v.getData()[i];
+				}
+			} else {
+				throw new Exception("The vectors dont match in size");
 			}
-			return new Vector(res);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		else {
-			System.err.println("The vectors dont match in size. Returning same vector");
-			return new Vector(u.getData());
-		}
-		
+		return new Vector(res);
 	}
-	
+
 	public Vector scalarAdd(float v) {
 		float[] res;
-		
-			res = new float[getSize()];
-			for(int i = 0;i < getSize();i++) {
-				res[i] = getData()[i] + v;
-			}
-			return new Vector(res);
-		
+
+		res = new float[getNumberOfDimensions()];
+		for (int i = 0; i < getNumberOfDimensions(); i++) {
+			res[i] = getData()[i] + v;
+		}
+		return new Vector(res);
+
 	}
-	
-	public static Vector scalarAdd(Vector v,float x) {
+
+	public static Vector scalarAdd(Vector v, float x) {
 		float[] res;
-		
-			res = new float[v.getSize()];
-			for(int i = 0;i < v.getSize();i++) {
-				res[i] = v.getData()[i] + x;
-			}
-			return new Vector(res);
-		
+
+		res = new float[v.getNumberOfDimensions()];
+		for (int i = 0; i < v.getNumberOfDimensions(); i++) {
+			res[i] = v.getData()[i] + x;
+		}
+		return new Vector(res);
+
 	}
-	
+
 	public Vector scalarMul(float v) {
 		float[] res;
-		
-			res = new float[getSize()];
-			for(int i = 0;i < getSize();i++) {
-				res[i] = getData()[i] * v;
-			}
-			return new Vector(res);
-		
+
+		res = new float[getNumberOfDimensions()];
+		for (int i = 0; i < getNumberOfDimensions(); i++) {
+			res[i] = getData()[i] * v;
+		}
+		return new Vector(res);
+
 	}
-	
-	public static Vector scalarMul(Vector v,float x) {
+
+	public static Vector scalarMul(Vector v, float x) {
 		float[] res;
-		
-			res = new float[v.getSize()];
-			for(int i = 0;i < v.getSize();i++) {
-				res[i] = v.getData()[i] * x;
-			}
-			return new Vector(res);
-		
+
+		res = new float[v.getNumberOfDimensions()];
+		for (int i = 0; i < v.getNumberOfDimensions(); i++) {
+			res[i] = v.getData()[i] * x;
+		}
+		return new Vector(res);
+
 	}
 
-
-	
 	public float[] getData() {
 		return data;
 	}
-	
+
 	public void setData(float[] data) {
 		this.data = data;
-		this.size = data.length;
+		this.numberOfDimensions = data.length;
 	}
-	
+
+	public float get(int index) {
+		return data[index];
+	}
+
+	public void setDataElement(int index, float val) {
+		data[index] = val;
+	}
+
+	public Vector cross(Vector v) {
+		float[] res = new float[3];
+		res[0] = (this.get(1) * v.get(2)) - (this.get(2) * v.get(1));
+		res[1] = -((this.get(0) * v.get(2)) - (this.get(2) * v.get(0)));
+		res[2] = (this.get(0) * v.get(1)) - (this.get(1) * v.get(0));
+		return new Vector(res);
+	}
+
+	public static Vector cross(Vector u, Vector v) {
+		float[] res = new float[3];
+		res[0] = (u.get(1) * v.get(2)) - (u.get(2) * v.get(1));
+		res[1] = -((u.get(0) * v.get(2)) - (u.get(2) * v.get(0)));
+		res[2] = (u.get(0) * v.get(1)) - (u.get(1) * v.get(0));
+		return new Vector(res);
+	}
+
 	public float dot(Vector v) {
 		float res = 0;
-		if(size == v.getSize()) {
-			for(int i = 0;i < v.getSize();i++) {
-				res += v.getData()[i] * this.getData()[i];
+
+		try {
+			if (numberOfDimensions == v.getNumberOfDimensions()) {
+				for (int i = 0; i < v.getNumberOfDimensions(); i++) {
+					res += v.getData()[i] * this.getData()[i];
+				}
+			} else {
+				throw new Exception("The vectors dont match in size");
 			}
-			return res;
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		else {
-			System.err.println("The vectors dont match in size. Returning 0");
-			return 0f;
-		}
+		return res;
 	}
-	
-	public static float dot(Vector u,Vector v) {
+
+	public static float dot(Vector u, Vector v) {
 		float res = 0;
-		if(u.getSize() == v.getSize()) {
-			for(int i = 0;i < v.getSize();i++) {
-				res += v.getData()[i] * u.getData()[i];
+
+		try {
+			if (u.getNumberOfDimensions() == v.getNumberOfDimensions()) {
+				for (int i = 0; i < v.getNumberOfDimensions(); i++) {
+					res += v.getData()[i] * u.getData()[i];
+				}
+			} else {
+				throw new Exception("The vectors dont match in size");
 			}
-			return res;
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		else {
-			System.err.println("The vectors dont match in size. Returning 0");
-			return 0f;
-		}
+		return res;
 	}
-	
+
 	public void display() {
-		for(int  i = 0;i < getSize();i++) {
-			System.out.println(getData()[i]);
+		System.out.print("[");
+		for (int i = 0; i < getNumberOfDimensions(); i++) {
+			System.out.print(getData()[i]);
+			if (i + 1 != getNumberOfDimensions()) {
+				System.out.print("\t");
+			}
 		}
+		System.out.print("]");
+		System.out.println();
 	}
-	
+
 	public static void display(Vector v) {
-		for(int  i = 0;i < v.getSize();i++) {
+		for (int i = 0; i < v.getNumberOfDimensions(); i++) {
 			System.out.println(v.getData()[i]);
 		}
 	}
+
+	public static Vector[] addDimensionToVec(Vector[] v, float val) {
+
+		Vector[] resData = new Vector[v.length];
+
+		for (int i = 0; i < v.length; i++) {
+			float[] tempDat = new float[v[i].getNumberOfDimensions() + 1];
+			for (int j = 0; j < v[i].getNumberOfDimensions(); j++) {
+				tempDat[j] = v[i].get(j);
+			}
+			tempDat[v[i].getNumberOfDimensions()] = val;
+			resData[i] = new Vector(tempDat);
+		}
+
+		return resData;
+	}
 	
+	public static Matrix addDimensionToVec(List<Vector> v, float val) {
+
+		Vector[] resData = new Vector[v.size()];
+
+		for (int i = 0; i < v.size(); i++) {
+			float[] tempDat = new float[v.get(i).getNumberOfDimensions() + 1];
+			for (int j = 0; j < v.get(i).getNumberOfDimensions(); j++) {
+				tempDat[j] = v.get(i).get(j);
+			}
+			tempDat[v.get(i).getNumberOfDimensions()] = val;
+			resData[i] = new Vector(tempDat);
+		}
+
+		return new Matrix(resData);
+	}
+
+	public Vector addDimensionToVec(float val) {
+
+		float[] tempDat = new float[this.getNumberOfDimensions() + 1];
+		
+		for (int j = 0; j < this.getNumberOfDimensions(); j++) {
+			tempDat[j] = this.get(j);
+		}
+		tempDat[this.getNumberOfDimensions()] = val;
+
+		return new Vector(tempDat);
+	}
+
+	public Vector getCopy() {
+		float[] res = new float[this.getNumberOfDimensions()];
+		
+		for(int i = 0;i < this.getNumberOfDimensions();i++) {
+			res[i] = this.get(i);
+		}
+		return new Vector(res);
+	}
+	
+	public String toString() {
+		String res = "";
+		res+="[";
+		for (int i = 0; i < getNumberOfDimensions(); i++) {
+			res+=(getData()[i]);
+			if (i + 1 != getNumberOfDimensions()) {
+				res+="    ";
+			}
+		}
+		res+="]";
+		return res;
+	}
+
 }
